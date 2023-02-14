8d9d715cbfd37900813f0f3546000165d7a12d7a
Emirhan Bilgiç <82952986+emirhanbilgic@users.noreply.github.com>
2023-02-14 16:07:55 +0300

    Create linkedList.py

---
+++ linkedList.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add_node(self, data):
+        new_node = Node(data)
+        if self.head is None: 
+           self.head = new_node
+        else:
+            current_node = self.head
+            while current_node.next is not None:
+                current_node = current_node.next
+            current_node.next = new_node
+
+    def print_list(self):
+        current_node = self.head
+        while current_node is not None:
+            print(current_node.data)
+            current_node = current_node.next
